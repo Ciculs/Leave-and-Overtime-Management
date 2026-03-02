f54727d7156edfe5aa0b76b5f8e7cd72a8f3c632
Hanh-HE180572 <nghoanganh218@gmail.com>
2026-03-02 15:18:12 +0700

    Fix conflix git

--- LeaveOTManagement/Controllers/OTController.cs
+++ LeaveOTManagement/Controllers/OTController.cs
@@ -1,58 +1,4 @@
-﻿//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Mvc;
-//using LeaveOTManagement.Services.Interfaces;
-//using LeaveOTManagement.DTOs.OT;
-
-//namespace LeaveOTManagement.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    //[Authorize]
-//    public class OTController : ControllerBase
-//    {
-//        private readonly IOTService _service;
-
-//        public OTController(IOTService service)
-//        {
-//            _service = service;
-//        }
-
-//        [HttpPost]
-//        public async Task<IActionResult> Create(CreateOtRequestDto dto)
-//        {
-//            var claim = User.FindFirst("UserId");
-//            if (claim == null)
-//                return Unauthorized();
-
-//            int userId = int.Parse(claim.Value);
-
-//            var id = await _service.CreateOtAsync(userId, dto);
-
-//            return Ok(new { id });
-//        }
-
-//        [HttpPut("{id}")]
-//        public async Task<IActionResult> Update(long id, UpdateOtRequestDto dto)
-//        {
-//            int userId = int.Parse(User.FindFirst("UserId")!.Value);
-
-//            await _service.UpdateOtAsync(id, userId, dto);
-
-//            return Ok();
-//        }
-
-//        [HttpGet]
-//        public async Task<IActionResult> GetMyOt([FromQuery] string? status)
-//        {
-//            int userId = int.Parse(User.FindFirst("UserId")!.Value);
-
-//            var result = await _service.GetMyOtAsync(userId, status);
-
-//            return Ok(result);
-//        }
-//    }
-//}
-
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using LeaveOTManagement.Services.Interfaces;
 using LeaveOTManagement.DTOs.OT;
@@ -61,6 +7,7 @@ namespace LeaveOTManagement.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class OTController : ControllerBase
     {
         private readonly IOTService _service;
@@ -70,13 +17,14 @@ namespace LeaveOTManagement.Controllers
             _service = service;
         }
 
-        // TEST MODE: hard-code userId
-        private int GetTestUserId() => 1;
-
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody] CreateOtRequestDto dto)
+        public async Task<IActionResult> Create(CreateOtRequestDto dto)
         {
-            var userId = GetTestUserId();
+            var claim = User.FindFirst("UserId");
+            if (claim == null)
+                return Unauthorized();
+
+            int userId = int.Parse(claim.Value);
 
             var id = await _service.CreateOtAsync(userId, dto);
 
@@ -84,9 +32,9 @@ namespace LeaveOTManagement.Controllers
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(long id, [FromBody] UpdateOtRequestDto dto)
+        public async Task<IActionResult> Update(long id, UpdateOtRequestDto dto)
         {
-            var userId = GetTestUserId();
+            int userId = int.Parse(User.FindFirst("UserId")!.Value);
 
             await _service.UpdateOtAsync(id, userId, dto);
 
@@ -96,7 +44,7 @@ namespace LeaveOTManagement.Controllers
         [HttpGet]
         public async Task<IActionResult> GetMyOt([FromQuery] string? status)
         {
-            var userId = GetTestUserId();
+            int userId = int.Parse(User.FindFirst("UserId")!.Value);
 
             var result = await _service.GetMyOtAsync(userId, status);
 
