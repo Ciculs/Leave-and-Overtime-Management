a9a2ea62f34e601bfab8ea0d05878d622dda8d62
Pham Thanh Dat <ciculscute221@gmail.com>
2026-03-02 10:44:12 +0700

    fix conflicts

--- LeaveOTManagement.FE/src/router/index.js
+++ LeaveOTManagement.FE/src/router/index.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import { createRouter, createWebHistory } from 'vue-router'
-
-import LeaveView from '../views/LeaveView.vue'
-
-import LeaveDashboardView from '../views/LeaveDashboardView.vue'
-import LeaveRequestFormView from '../views/LeaveRequestFormView.vue'
-
-const routes = [
-  {
-    path: '/',
-    redirect: '/leave' 
-  },
-  {
-    path: '/leave-test', 
-    name: 'LeaveTest',
-    component: LeaveView
-  },
-  {
-    path: '/leave',
-    name: 'LeaveDashboard',
-    component: LeaveDashboardView
-  },
-  {
-    path: '/leave/request',
-    name: 'LeaveRequest',
-    component: LeaveRequestFormView
-=======
 import { createRouter, createWebHistory } from "vue-router"
 import DashboardLayout from "../layouts/DashboardLayout.vue"
 
@@ -65,7 +37,6 @@ const routes = [
         meta: { role: "Employee" }  
       }
     ]
->>>>>>> main
   }
 ]
 
