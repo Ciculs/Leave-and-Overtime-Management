73ef9ebd1c1cd2f6d92d8add7f5fa7ebde51125d
truongcvqhe182358 <truongcvqhe182358@fpt.edu.vn>
2026-03-06 10:30:06 +0700

    Fix: add readd totalday into CreateLeaveDto.cs

--- LeaveOTManagement/DTOs/CreateLeaveDto.cs
+++ LeaveOTManagement/DTOs/CreateLeaveDto.cs
@@ -8,13 +8,15 @@ namespace LeaveOTManagement.DTOs
         public int LeaveTypeId { get; set; }
 
         [Required]
-        public DateOnly FromDate { get; set; }
+        public DateOnly FromDate { get; set; } 
 
         [Required]
-        public DateOnly ToDate { get; set; }
+        public DateOnly ToDate { get; set; }   
 
         [Required]
-        [MaxLength(1000, ErrorMessage = "Lý do không được quá 1000 ký tự")]
+        public decimal TotalDays { get; set; }
+
+        [Required]
         public string Reason { get; set; }
     }
 }
